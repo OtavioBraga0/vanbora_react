cd0f17f90e950191d4aa320f45a59d91ee731235
amexassessoria <amexassessoria1@gmail.com>
2019-09-09 18:29:19 -0300

    Finalizado Listragem de Grupos na Home do Aluno

--- src/screens/HomeAlunoScreen.js
+++ src/screens/HomeAlunoScreen.js
@@ -4,29 +4,66 @@ import FirebaseService from "../../service/FirebaseService";
 
 export default class HomeAlunoScreen extends Component {
     state = {
-        dataList: null,
+        dataList: [],
+        alunoId: 0,
     };
 
     static navigationOptions = {
         title: 'Home - Aluno',
     };
 
+    // VALIDA DE O PASSAGEIRO JÁ EXISTE NA LISTAGEM, CASO EXISTA, SUBSTITUE OS DADOS ANTIGOS COM OS NOVOS 
+    verifyDataList(dataIn){        
+        let dataList = this.state.dataList;   
+        let exist = 0;   
+        if(dataIn.length != 0){
+            dataList.forEach((element, index) => {      
+                if(dataIn[0].key == element.key){       
+                    exist += 1;
+                    // 1º ARGUMENTO -> REMOVE ITENS APARETIDO DO INDEX PASSADO
+                    // 2º ARGUMENTO -> LIMITE DE ITENS QUE DEVEM SER REMOVIDOS
+                    // 3º ARGUMENTO -> SUBSTITUI O ITEM REMOVIDO PELO NOVO PASSADO
+                    dataList.splice(index, 1, dataIn[0])
+                }
+            })
+        }
+
+        if(exist == 0){
+            dataList.push(dataIn[0]);
+            this.setState({dataList: dataList})
+        } else {
+            this.setState({dataList: dataList})
+        }
+    }
+
     async componentDidMount() {
         const key = await AsyncStorage.getItem("@Usuario:key");
-        FirebaseService.getDataWithKey('usuario',key, dataIn => {
-            // FirebaseService.getDataWithChild('grupo', 'usuario/'+key, 'usuario/'+key, dataIn => {console.log(dataIn)});
-            console.log(dataIn[0].grupo)
-        })
+        this.setState({alunoId: key});
+        // LISTA A KEY DE TODOS OS GRUPOS EM QUE O ALUNO ESTA
+        FirebaseService.getDataList(`usuario/${this.state.alunoId}/grupo` , dataIn => {
+            // PERCORRE TODO O RETORNO DAS KEYs           
+            dataIn.forEach(grupo => {                        
+                // RETORNA OS DADOS DOS GRUPOS BASEADOS NAS KEYs RECOLHIDAS ANTERIORMENTE
+                FirebaseService.getDataWithKey(
+                    'grupo',
+                    grupo.key, 
+                    dataIn => {
+                        // CHAMA A FUNÇÃO QUE VALIDA O DATALIST
+                        this.verifyDataList(dataIn);                        
+                    });
+            });
+        });
     };
 
     render() {
-        const {dataList} = this.state;
+        const {dataList, alunoId} = this.state;
 
         return(
             <View>
                 <ScrollView>
                     <View style={styles.fullWidth}>
                         {
+                            // PERCORRE TODO DATALIST COM OS DADOS ATUALIZADOS RENDERIZANDO-OS NA TELA
                             dataList && dataList.map(
                                 (item, index) => {
                                     return (
@@ -37,6 +74,9 @@ export default class HomeAlunoScreen extends Component {
 
                                                 <Text style={styles.listItemHeader}> Período </Text>
                                                 <Text style={styles.listItemText}> {item.periodo} </Text>
+
+                                                <Text style={styles.listItemHeader}> Presença </Text>
+                                                <Text style={styles.listItemText}> {item.usuario[alunoId].presenca} </Text>
                                             </View>
                                         </View>
                                     );
