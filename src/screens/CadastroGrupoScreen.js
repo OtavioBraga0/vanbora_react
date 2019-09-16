3797da6ac16118dc9f068a128175a24814a8ddf5
aluno7 <aluno7@unilab.net>
2019-09-16 20:12:08 -0300

    Refatorado Tela de Cadastro de Grupo

--- src/screens/CadastroGrupoScreen.js
+++ src/screens/CadastroGrupoScreen.js
@@ -1,68 +1,61 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { StyleSheet, Button, Text, TextInput, View, Picker, AsyncStorage } from "react-native";
 import FirebaseService from "../../service/FirebaseService";
 
-export default class CadastroScreen extends Component { 
-    static navigationOptions = {
-        title: 'Cadastro',
+const CadastroGrupoScreen = ({navigation}) => {
+    const {nome, setNome} = useState("");
+    const {periodo, setPeriodo} = useState("matutino");
+
+    navigationOptions = {
+        title: "Cadastro - Grupo"
     };
+    submit = async () => {
+        if (nome == "") {
+            alert("Preencha todos os campos!");
+        } else {
+            try {
+                const key = await AsyncStorage.getItem("@Usuario:key");
 
-    state = {
-        nome: "",
-        periodo: "matutino"
-    };
+                FirebaseService.pushData('grupo', {
+                    nome,
+                    periodo,
+                    key
+                });
 
-    render(){
-        const {nome, periodo} = this.state;
-        const {replace} = this.props.navigation;
-
-        submit = async () => {
-            if (nome == "") {
-                alert("Preencha todos os campos!");
-            } else {
-                try {
-                    const motoristaId = await AsyncStorage.getItem("@Usuario:key");
-
-                    FirebaseService.pushData('grupo', {
-                        nome,
-                        periodo,
-                        motoristaId
-                    });
-
-                    alert("Novo grupo criado!");
-                    replace("HomeMotorista");
-                } catch (error) {
-                    alert("Erro de conexão")
-                }
+                alert("Novo grupo criado!");
+                navigation.replace("HomeMotorista");
+            } catch (error) {
+                alert("Erro de conexão")
             }
-        };
-        return(
-            <View style={{marginHorizontal: 15}}>
-                <Text>Nome do Grupo</Text>
-                <TextInput
-                    style={styles.input}
-                    value={nome}
-                    required
-                    onChangeText={(value) => this.setState({nome: value})}/>
-                <Text>Período</Text>
-                <Picker
-                    style={styles.input}
-                    selectedValue={periodo}
-                    onValueChange={(itemValue, itemIndex) =>
-                        this.setState({periodo: itemValue})
-                    }>
-                    <Picker.Item label="Motutino" value="matutino" />
-                    <Picker.Item label="Vespertino" value="vespertino" />
-                    <Picker.Item label="Noturno" value="noturno" />
-                </Picker>
-                <Button 
-                    type="button"
-                    title="Cadastrar"
-                    onPress={() => submit()}
-                    style={{marginTop: '20px', display: 'inline-block'}} />
-            </View>
-        );
-    }
+        }
+    };
+
+    return(
+        <View style={{marginHorizontal: 15}}>
+            <Text>Nome do Grupo</Text>
+            <TextInput
+                style={styles.input}
+                value={nome}
+                required
+                onChangeText={(value) => setNome(value)}/>
+            <Text>Período</Text>
+            <Picker
+                style={styles.input}
+                selectedValue={periodo}
+                onValueChange={(itemValue, itemIndex) =>
+                    setPeriodo(itemValue)
+                }>
+                <Picker.Item label="Motutino" value="matutino" />
+                <Picker.Item label="Vespertino" value="vespertino" />
+                <Picker.Item label="Noturno" value="noturno" />
+            </Picker>
+            <Button 
+                type="button"
+                title="Cadastrar"
+                onPress={() => submit()}
+                style={{marginTop: '20px', display: 'inline-block'}} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
