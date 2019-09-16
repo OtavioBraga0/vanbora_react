dd053325956ae3a3d69c7205a51d81a0ae8fa820
aluno7 <aluno7@unilab.net>
2019-09-16 20:02:54 -0300

    Refatorado Tela de Cadastro de Alunos

--- src/screens/CadastroAlunoScreen.js
+++ src/screens/CadastroAlunoScreen.js
@@ -1,70 +1,64 @@
-import React, { Component } from "react";
-import { StyleSheet, Button, Text, TextInput, View, Picker, AsyncStorage } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Button, Text, TextInput, View } from "react-native";
 import FirebaseService from "../../service/FirebaseService";
 
-export default class CadastroScreen extends Component { 
-    static navigationOptions = {
-        title: 'Cadastro - Aluno',
+const CadastroAlunoScreen = ({navigation}) => {
+    const {telefone, setTelefone} = useState("");
+    navigationOptions = {
+        title: "Cadastro - Aluno"
     };
 
-    state = {
-        telefone: "",
-    };
+    submit = async () => {
+        if (telefone == "") {
+            alert("Preencha todos os campos!");
+        } else {
+            try {
+                let grupoId = this.props.navigation.getParam("grupoId");
+                FirebaseService.getDataWithChild("usuario", "telefone", telefone, dataIn => {
 
-    render(){
-        const {telefone} = this.state;
-        const {goBack} = this.props.navigation;
+                    let objUsuario = {
+                        [grupoId]: {
+                            presenca: "S",                                
+                        }, 
+                    }
 
+                    let objGrupo = {
+                        [dataIn[0].key]: {
+                            presenca: "S",
+                        } 
+                    }
 
-        submit = async () => {
-            if (telefone == "") {
-                alert("Preencha todos os campos!");
-            } else {
-                try {
-                    let grupoId = this.props.navigation.getParam("grupoId");
-                    FirebaseService.getDataWithChild("usuario", "telefone", telefone, dataIn => {
+                    FirebaseService.updateRelationship(`usuario/${dataIn[0].key}/grupo`, objUsuario)
+                    FirebaseService.updateRelationship(`grupo/${grupoId}/usuario`, objGrupo)
 
-                        let objUsuario = {
-                            [grupoId]: {
-                                presenca: "S",                                
-                            }, 
-                        }
+                    alert("Aluno adicionado!");
+                    navigation.goBack();
+                })
 
-                        let objGrupo = {
-                            [dataIn[0].key]: {
-                                presenca: "S",
-                            } 
-                        }
-
-                        FirebaseService.updateRelationship(`usuario/${dataIn[0].key}/grupo`, objUsuario)
-                        FirebaseService.updateRelationship(`grupo/${grupoId}/usuario`, objGrupo)
-
-                        alert("Aluno adicionado!");
-                        goBack();
-                    })
-
-                } catch (error) {
-                    alert("Erro de conexão")
-                }
+            } catch (error) {
+                alert("Erro de conexão")
             }
-        };
-        return(
-            <View style={{marginHorizontal: 15}}>
-                <Text>Telefone do Aluno</Text>
-                <TextInput
-                    style={styles.input}
-                    value={telefone}
-                    required
-                    onChangeText={(value) => this.setState({telefone: value})}/>
-                <Button 
-                    type="button"
-                    title="Cadastrar"
-                    onPress={() => submit()}
-                    style={{marginTop: '20px', display: 'inline-block'}} />
-            </View>
-        );
-    }
+        }
+    };
+
+    return(
+        <View style={{marginHorizontal: 15}}>
+            <Text>Telefone do Aluno</Text>
+            <TextInput
+                style={styles.input}
+                value={telefone}
+                required
+                onChangeText={(value) => setTelefone(value)}/>
+            <Button 
+                type="button"
+                title="Cadastrar"
+                onPress={() => submit()}
+                style={{marginTop: '20px', display: 'inline-block'}} />
+        </View>
+    );
 }
+
+export default CadastroAlunoScreen;
 
 const styles = StyleSheet.create({
     input: {
