7a8cdb5fc3078e831dd883d04aa3a0b9a403650d
aluno7 <aluno7@unilab.net>
2019-09-16 22:09:42 -0300

    Refatora Tela de Perfil do Usuário

--- src/screens/PerfilScreen.js
+++ src/screens/PerfilScreen.js
@@ -1,68 +1,66 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { StyleSheet, Button, Text, TextInput, View, AsyncStorage } from "react-native";
 import FirebaseService from "../../service/FirebaseService";
 
-export default class PerfilScreen extends Component { 
-    static navigationOptions = {
+const PerfilScreen = ({navigation}) => {
+    const {key, setKey} = useState("");
+    const {nome, setNome} = useState("");
+    const {telefone, setTelefone} = useState("");
+    const {tipo, setTipo} = useState("");
+    const {grupo, setGrupo} = useState(null);
+
+    navigationOptions = {
         title: 'Perfil',
     };
 
-    state = {
-        telefone: "",
-    };
-
-    async componentDidMount(){
+    const init = async () => {
         const key = await AsyncStorage.getItem("@Usuario:key");
         FirebaseService.getDataWithKey("usuario",key, (dataIn) => this.setState(dataIn[0]));        
     }
 
-    render(){
-        const {key, nome, telefone, tipo, grupo = null} = this.state;
-        const {goBack} = this.props.navigation;
-
-
-        submit = async () => {
-            if (telefone == "" && nome == "") {
-                alert("Preencha todos os campos!");
-            } else {
-                try {
-                    let obj = {
-                        "nome": nome,
-                        "telefone": telefone,
-                        "tipo": tipo,
-                        "grupo": grupo,
-                    }
-                    FirebaseService.changeValues(`usuario/${key}`, obj)
-
-                    alert("Perfil atualizado!")
-                    goBack();
-                } catch (error) {
-                    alert("Erro de conexão")
+    submit = async () => {
+        if (telefone == "" && nome == "") {
+            alert("Preencha todos os campos!");
+        } else {
+            try {
+                let obj = {
+                    "nome": nome,
+                    "telefone": telefone,
+                    "tipo": tipo,
+                    "grupo": grupo,
                 }
+                FirebaseService.changeValues(`usuario/${key}`, obj)
+
+                alert("Perfil atualizado!")
+                navigation.goBack();
+            } catch (error) {
+                alert("Erro de conexão")
             }
-        };
-        return(
-            <View style={{marginHorizontal: 15}}>
-                <Text>Nome</Text>
-                <TextInput
-                    style={styles.input}
-                    value={nome}
-                    required
-                    onChangeText={(value) => this.setState({nome: value})}/>
-                <Text>Telefone</Text>
-                <TextInput
-                    style={styles.input}
-                    value={telefone}
-                    required
-                    onChangeText={(value) => this.setState({telefone: value})}/>
-                <Button 
-                    type="button"
-                    title="Cadastrar"
-                    onPress={() => submit()}
-                    style={{marginTop: '20px', display: 'inline-block'}} />
-            </View>
-        );
-    }
+        }
+    };
+
+    init();
+    return(
+        <View style={{marginHorizontal: 15}}>
+            <Text>Nome</Text>
+            <TextInput
+                style={styles.input}
+                value={nome}
+                required
+                onChangeText={(value) => setNome(value)}/>
+            <Text>Telefone</Text>
+            <TextInput
+                style={styles.input}
+                value={telefone}
+                required
+                onChangeText={(value) => setTelefone(value)}/>
+            <Button 
+                type="button"
+                title="Cadastrar"
+                onPress={() => submit()}
+                style={{marginTop: '20px', display: 'inline-block'}} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -74,3 +72,5 @@ const styles = StyleSheet.create({
         padding: 5,
     }
 });
+
+export default PerfilScreen;
